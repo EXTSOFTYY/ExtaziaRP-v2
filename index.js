79623d51a85d57b1cfdd988d7c842129b1ace185
EXTSOFTYY <44620409+EXTSOFTYY@users.noreply.github.com>
2019-03-13 21:12:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -415,17 +415,6 @@ bot.on('message', message => {
 
     // -> ci d'identité - UPDATE 2.0
 
-    if (message.content === prefix + "ci Thibault Savastano"){
-        message.delete();
-        message.channel.send("Voici la ci d'identité de Thibault Savastano:", {
-            file: "https://zupimages.net/up/19/11/620k.jpg"
-        });
-        console.log("[BOT ExtaziaRP] LOGS -> Un utilisateur demande la commande -> /ci Thibault Savastano [Succès].");
-        
-    }
-
-    // -> ci d'identité - UPDATE 2.0
-
     if (message.content === prefix + "ci Thomas Shelby"){
         message.delete();
         message.channel.send("Voici la ci d'identité de Thomas Shelby:", {
