711135a35a37277f0508df47b48250858fac01e8
EXTSOFTYY <44620409+EXTSOFTYY@users.noreply.github.com>
2019-03-16 17:38:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -478,6 +478,17 @@ bot.on('message', message => {
         console.log("[BOT ExtaziaRP] LOGS -> Un utilisateur demande la commande -> /ci Ronald Macway [Succès].");
         
     }
+    
+    // -> ci d'identité - UPDATE 2.0
+
+    if (message.content === prefix + "ci Cyana Levy"){
+        message.delete();
+        message.channel.send("Voici la ci d'identité de Cyana Levy:", {
+            file: "https://zupimages.net/up/19/11/ay85.jpg"
+        });
+        console.log("[BOT ExtaziaRP] LOGS -> Un utilisateur demande la commande -> /ci Cyana Levy [Succès].");
+        
+    }
 
 });
 
