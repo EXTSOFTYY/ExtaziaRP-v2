8dd4b31020986543e3c849837696f3549e4c69f1
EXTSOFTYY <44620409+EXTSOFTYY@users.noreply.github.com>
2019-03-24 13:02:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -413,6 +413,17 @@ bot.on('message', message => {
         
     }
     
+    // -> ci d'identité - UPDATE 2.0
+
+    if (message.content === prefix + "ci Aleksander Jerzmanowski"){
+        message.delete();
+        message.channel.send("Voici la ci d'identité d'Aleksander Jerzmanowski:", {
+            file: "https://zupimages.net/up/19/12/hi61.jpg"
+        });
+        console.log("[BOT ExtaziaRP] LOGS -> Un utilisateur demande la commande -> /ci Aleksander Jerzmanowski [Succès].");
+        
+    }
+    
 });
 
 bot.login(process.env.TOKEN);
