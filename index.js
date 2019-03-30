d66fbf0c82da30f713b4facf411293261b4dde2d
EXTSOFTYY <44620409+EXTSOFTYY@users.noreply.github.com>
2019-03-30 18:15:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -382,28 +382,6 @@ bot.on('message', message => {
     
     // -> ci d'identité - UPDATE 2.0
 
-    if (message.content === prefix + "ci Leonardo Vietti"){
-        message.delete();
-        message.channel.send("Voici la ci d'identité de Leonardo Vietti:", {
-            file: "https://zupimages.net/up/19/12/7hdy.jpg"
-        });
-        console.log("[BOT ExtaziaRP] LOGS -> Un utilisateur demande la commande -> /ci Leonardo Vietti [Succès].");
-        
-    }
-    
-    // -> ci d'identité - UPDATE 2.0
-
-    if (message.content === prefix + "ci Mario Vietti"){
-        message.delete();
-        message.channel.send("Voici la ci d'identité de Mario Vietti:", {
-            file: "https://zupimages.net/up/19/12/yivh.jpg"
-        });
-        console.log("[BOT ExtaziaRP] LOGS -> Un utilisateur demande la commande -> /ci Mario Vietti [Succès].");
-        
-    }
-    
-    // -> ci d'identité - UPDATE 2.0
-
     if (message.content === prefix + "ci Aleksander Jerzmanowski"){
         message.delete();
         message.channel.send("Voici la ci d'identité d'Aleksander Jerzmanowski:", {
