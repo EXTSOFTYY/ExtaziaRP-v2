4a1a84e91c6c1d304bb346408a3c0332652beb77
EXTSOFTYY <44620409+EXTSOFTYY@users.noreply.github.com>
2019-05-01 12:29:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -325,6 +325,28 @@ bot.on('message', message => {
         
     }
     
+    // -> ci d'identité - UPDATE 2.0
+
+    if (message.content === prefix + "ci Kirill Sopoz"){
+        message.delete();
+        message.channel.send("Voici la ci d'identité de Kirill Sopoz:", {
+            file: "https://zupimages.net/up/19/18/8kc0.jpg"
+        });
+        console.log("[BOT ExtaziaRP] LOGS -> Un utilisateur demande la commande -> /ci Kirill Sopoz [Succès].");
+        
+    }
+    
+    // -> ci d'identité - UPDATE 2.0
+
+    if (message.content === prefix + "ci Saalva Sopoz"){
+        message.delete();
+        message.channel.send("Voici la ci d'identité de Saalva Sopoz:", {
+            file: "https://zupimages.net/up/19/18/9qoo.jpg"
+        });
+        console.log("[BOT ExtaziaRP] LOGS -> Un utilisateur demande la commande -> /ci Saalva Sopoz [Succès].");
+        
+    }
+    
 });
 
 bot.login(process.env.TOKEN);
