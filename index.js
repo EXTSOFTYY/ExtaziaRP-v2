9910d3e3bc6082612da35c43104c2d1e994fe4bc
EXTSOFTYY <44620409+EXTSOFTYY@users.noreply.github.com>
2019-05-13 16:14:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -459,12 +459,12 @@ bot.on('message', message => {
     
     // -> ci d'identité - UPDATE 2.0
 
-    if (message.content === prefix + "ci Santago Salvador"){
+    if (message.content === prefix + "ci Sergio Simeone"){
         message.delete();
-        message.channel.send("Voici la ci d'identité de Santago Salvador:", {
-            file: "https://zupimages.net/up/19/19/vv98.jpg"
+        message.channel.send("Voici la ci d'identité de Sergio Simeone:", {
+            file: "https://zupimages.net/up/19/19/zlz0.jpg"
         });
-        console.log("[BOT ExtaziaRP] LOGS -> Un utilisateur demande la commande -> /ci Santago Salvador [Succès].");
+        console.log("[BOT ExtaziaRP] LOGS -> Un utilisateur demande la commande -> /ci Sergio Simeone [Succès].");
         
     }
     
