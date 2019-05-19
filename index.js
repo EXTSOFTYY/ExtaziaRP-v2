0e82d43dd22afc02cf6de089f9555a998a5fe229
EXTSOFTYY <44620409+EXTSOFTYY@users.noreply.github.com>
2019-05-19 21:41:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -473,7 +473,7 @@ bot.on('message', message => {
     if (message.content === prefix + "ci Lucas Talavera"){
         message.delete();
         message.channel.send("Voici la ci d'identité de Lucas Talavera:", {
-            file: "https://zupimages.net/up/19/20/8f6a.jpg"
+            file: "https://zupimages.net/up/19/20/jdkr.jpg"
         });
         console.log("[BOT ExtaziaRP] LOGS -> Un utilisateur demande la commande -> /ci Lucas Talavera [Succès].");
         
