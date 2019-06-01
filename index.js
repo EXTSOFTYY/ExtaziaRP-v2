f45fd9eff1a4741a4e4cde8a47435a7017129b65
EXTSOFTYY <44620409+EXTSOFTYY@users.noreply.github.com>
2019-06-01 20:51:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -567,6 +567,17 @@ bot.on('message', message => {
         
     }
     
+    // -> ci d'identité - UPDATE 2.0
+
+    if (message.content === prefix + "ci Reagan Johnson"){
+        message.delete();
+        message.channel.send("Voici la ci d'identité de Reagan Johnson:", {
+            file: "https://zupimages.net/up/19/22/50fn.jpg"
+        });
+        console.log("[BOT ExtaziaRP] LOGS -> Un utilisateur demande la commande -> /ci Reagan Johnson [Succès].");
+        
+    }
+    
 });
 
 bot.login(process.env.TOKEN);
