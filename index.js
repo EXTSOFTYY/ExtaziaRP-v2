c00f1459584d18438be33fdc2cf87ada1eb7b4d5
EXTSOFTYY <44620409+EXTSOFTYY@users.noreply.github.com>
2019-06-09 01:28:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -484,7 +484,7 @@ bot.on('message', message => {
     if (message.content === prefix + "ci Lucas Black"){
         message.delete();
         message.channel.send("Voici la ci d'identité de Lucas Black:", {
-            file: "https://zupimages.net/up/19/23/8k0i.jpg"
+            file: "https://zupimages.net/up/19/23/b5kz.jpg"
         });
         console.log("[BOT ExtaziaRP] LOGS -> Un utilisateur demande la commande -> /ci Lucas Black [Succès].");
         
